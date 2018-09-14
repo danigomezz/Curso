05df2bc909c40202a21a97f97306cfaea2e7c384
danigomezz <43217856+danigomezz@users.noreply.github.com>
2018-09-14 16:31:00 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -10,8 +10,67 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-       echo 'HOLAA';
+        <h1>
+      <?php
+        $primaveraVariable= 2;
+        echo $primaveraVariable. "<br>";
+        $segundaVariable=12345;
+        echo $segundaVariable;
         ?>
-    </body>
+        </h1>
+        <p>
+        <h2>
+            <?php
+           //sumar datos 
+              echo 'esto es otra seccion';
+               $primaveraVariable= 2;
+                $segundaVariable=5;
+                $lenguaje='javascript';
+                echo '<br>';
+                echo $primaveraVariable + $segundaVariable; 
+                if ($primaveraVariable < 1){
+                echo '<br>';    
+               echo 'soy menor';
+               }
+            else {
+                   echo 'soy mayor';
+                 }
+                 echo '<br>';
+                 switch ($lenguaje) {
+                     case "php":
+                         echo 'php';
+                         break;
+                     case "javascript":
+                         echo 'javascript';
+                         break;
+                     case "html":
+                         echo 'html';
+                         break;
+                   }
+                   
+                   $arreglo=[2, "HOLA",5];
+                   echo $arreglo[1];
+            ?>
+                    </p>
+            </h2>
+            <pre>
+            <h3>
+                <?php
+                     print_r($arreglo);
+                 ?>
+         </h3>
+           </pre>
+        
+        <pre>
+            <h3>
+                <?php
+                     var_dump($arreglo);
+                     echo '<br>';
+                     $arreglo[]='phyton';
+                     var_dump($arreglo);
+                     
+                 ?>
+         </h3>
+           </pre>
+     </body>
 </html>
